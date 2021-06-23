f4c1e2256d30e5dfab1a66176fd0421e86c6a947
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-06-23 17:17:15 -0300

    adaptando para 996px

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,12 @@ import styles from "./app.module.css";
 
 import Star from "./components/Star";
 
+import moonImg from "./assets/moon.png";
 import landImg from "./assets/land.png";
 import googleLogoImg from "./assets/google-logo.png";
+
+import HeroSvg from "./components/HeroSvg";
+import Points from "./components/Points";
 
 export default function App(): ReactElement {
   const [drawerMenuShown, setDrawerMenuShown] = useState(false);
@@ -45,90 +49,122 @@ export default function App(): ReactElement {
 
   return (
     <>
-      <header>
-        <div id={styles.navigation}>
-          <div id={styles.logo}>
-            <div id={styles.logoIcon}>LOGO</div>
-            <span id={styles.logoText}>Bug Tracker</span>
-          </div>
+      <div id={styles.fitScreen} className="noCssModule_fitScreen">
+        <header>
+          <div id={styles.navigation}>
+            <div id={styles.logo}>
+              <div id={styles.logoIcon}>LOGO</div>
+              <span id={styles.logoText}>Bug Tracker</span>
+            </div>
 
-          <div id={styles.hamburgerButtonWrapper}>
-            <button
-              id={styles.hamburgerButton}
-              className={drawerMenuShown ? styles.fixed : ""}
-              type="button"
-              onClick={toggleDropdownMenu}
-            >
-              <FiMenu
-                size={32}
-                color="var(--light)"
-                className={
-                  drawerMenuShown
-                    ? styles.hamburgerClose
-                    : styles.hamburgerNormal
-                }
-              />
-            </button>
-          </div>
-        </div>
+            <div id={styles.hamburgerButtonWrapper}>
+              <button
+                id={styles.hamburgerButton}
+                className={drawerMenuShown ? styles.fixed : ""}
+                type="button"
+                onClick={toggleDropdownMenu}
+              >
+                <FiMenu
+                  size={32}
+                  color="var(--light)"
+                  className={
+                    drawerMenuShown
+                      ? styles.hamburgerClose
+                      : styles.hamburgerNormal
+                  }
+                />
+              </button>
+            </div>
 
-        {drawerMenuShown && (
-          <>
-            <div id={styles.drawerMenuOutside} ref={drawerMenuOutsideRef}>
-              <div id={styles.drawerMenu}>
-                <div className={styles.drawerItem}>
-                  <button type="button" className={styles.signInButton}>
-                    Sign In
-                  </button>
-                </div>
-                <div className={styles.drawerItem}>
-                  <button type="button" className={styles.signUpButton}>
-                    Sign Up
-                  </button>
-                </div>
-                <div className={styles.drawerItem}>
-                  <button type="button" className={styles.languageButton}>
-                    <FiGlobe size={28} color="var(--light)" />
+            <div id={styles.navbar}>
+              <div className={styles.navitem}>
+                <button type="button" className={styles.languageButton}>
+                  <FiGlobe size={28} color="var(--light)" />
 
-                    <strong>Idioma</strong>
-                  </button>
-                </div>
+                  <strong>Idioma</strong>
+                </button>
+              </div>
+
+              <div className={styles.navitem}>
+                <button type="button" className={styles.signInButton}>
+                  Sign In
+                </button>
+              </div>
+
+              <div className={styles.navitem}>
+                <button type="button" className={styles.signUpButton}>
+                  Sign Up
+                </button>
               </div>
             </div>
-          </>
-        )}
-      </header>
+          </div>
 
-      <main>
-        <h1 id={styles.introductionTitle}>
-          Work on big ideas, without the busywork.
-        </h1>
-        <span id={styles.introductionDescription}>
-          Collaborate, manage projects, and reach new productivity peaks. From
-          high rises to the home office, the way your team works is unique
-          accomplish it all with Trello.
-        </span>
-        <div id={styles.introductionButtonContainer}>
-          <button
-            id={styles.introductionButton}
-            className={styles.signUpButton}
-            type="button"
-          >
-            SIGN UP
-          </button>
-        </div>
-      </main>
+          {drawerMenuShown && (
+            <>
+              <div id={styles.drawerMenuOutside} ref={drawerMenuOutsideRef}>
+                <div id={styles.drawerMenu}>
+                  <div className={styles.drawerItem}>
+                    <button type="button" className={styles.signInButton}>
+                      Sign In
+                    </button>
+                  </div>
+                  <div className={styles.drawerItem}>
+                    <button type="button" className={styles.signUpButton}>
+                      Sign Up
+                    </button>
+                  </div>
+                  <div className={styles.drawerItem}>
+                    <button type="button" className={styles.languageButton}>
+                      <FiGlobe size={28} color="var(--light)" />
+
+                      <strong>Idioma</strong>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+        </header>
+
+        <main>
+          <div>
+            <h1 id={styles.introductionTitle}>
+              Work on big ideas, without the busywork.
+            </h1>
+            <span id={styles.introductionDescription}>
+              Collaborate, manage projects, and reach new productivity peaks.
+              From high rises to the home office, the way your team works is
+              unique accomplish it all with Trello.
+            </span>
+            <div id={styles.introductionButtonContainer}>
+              <Points />
+
+              <button
+                id={styles.introductionButton}
+                className={styles.signUpButton}
+                type="button"
+              >
+                SIGN UP
+              </button>
+            </div>
+          </div>
+          <div id={styles.introductionImgContainer}>
+            <HeroSvg />
+          </div>
+        </main>
+      </div>
 
       <section id={styles.pitchSection} className={styles.sectionMargin}>
         <Star top="1rem" left="36%" delay={0.1} />
-        <Star top="90%" left="10rem" delay={0.4} />
-        <Star top="3rem" left="85%" delay={0.9} />
-        <Star top="4rem" left="33%" delay={0.56} />
+        <Star top="90%" left="10rem" delay={1.3} />
+        <Star top="3rem" left="75%" delay={0.9} />
+        <Star top="4rem" left="20%" delay={0.56} />
         <Star top="85%" left="80%" delay={0.45} />
         <Star top="86%" left="10%" />
-        <Star top="14rem" left="33%" delay={0.33} />
+        <Star top="14%" left="5%" delay={0.33} />
 
         <div>
+          <img src={moonImg} id={styles.moon} />
           <h1>
             Learn more about our <span>work</span>
           </h1>
@@ -136,15 +172,17 @@ export default function App(): ReactElement {
         </div>
       </section>
 
-      <section className={styles.sectionMargin}>
+      <section id={styles.showcaseSection} className={styles.sectionMargin}>
         <div className={styles.showcase}>
-          <h2 className={styles.showcaseTitle}>
-            Manage everything in one workspace
-          </h2>
-          <p className={styles.showcaseDescription}>
-            Planning, tracking, and delivering your team’s best work has never
-            been easier
-          </p>
+          <div className={styles.showcaseTextContainer}>
+            <h2 className={styles.showcaseTitle}>
+              Manage everything in one workspace
+            </h2>
+            <p className={styles.showcaseDescription}>
+              Planning, tracking, and delivering your team’s best work has never
+              been easier
+            </p>
+          </div>
           <img
             className={styles.showcaseImg}
             src="https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.jpg"
@@ -153,11 +191,13 @@ export default function App(): ReactElement {
           />
         </div>
         <div className={styles.showcase}>
-          <h2 className={styles.showcaseTitle}>Set up in minutes</h2>
-          <p className={styles.showcaseDescription}>
-            Get started fast with hundreds of visual and customizable templates
-            - or create your own
-          </p>
+          <div className={styles.showcaseTextContainer}>
+            <h2 className={styles.showcaseTitle}>Set up in minutes</h2>
+            <p className={styles.showcaseDescription}>
+              Get started fast with hundreds of visual and customizable
+              templates - or create your own
+            </p>
+          </div>
           <img
             className={styles.showcaseImg}
             src="https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.jpg"
@@ -244,15 +284,16 @@ export default function App(): ReactElement {
           <img src={landImg} alt="" />
         </div>
 
-        <Star top="90%" left="20%" delay={0.1} />
-        <Star top="51%" left="20%" delay={0.2} />
-        <Star top="51%" left="60%" delay={0.8} />
+        <Star top="32%" left="38%" delay={0.1} />
+        <Star top="38%" left="94%" delay={0.2} />
+        <Star top="38%" left="0.5rem" delay={0.2} />
+        <Star top="18%" left="72%" delay={0.2} />
+        <Star top="30%" left="60%" delay={0.8} />
         <Star top="20%" left="20%" delay={0.1} />
         <Star top="15%" left="90%" delay={0.7} />
         <Star top="15%" left="50%" delay={0.9} />
-        <Star top="1rem" left="20%" delay={0.4} />
-        <Star top="1rem" left="10%" delay={0.5} />
-        <Star top="80%" left="50%" delay={0.3} />
+        <Star top="8rem" left="2rem" delay={0.4} />
+        <Star top="4rem" left="10%" delay={0.5} />
       </section>
 
       <footer>
