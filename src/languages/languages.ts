6b11f94fc2f8d06784a67d6fc54509501ebffa2d
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-07-24 19:32:37 -0300

    adicionando import do svg no objeto de linguagens

--- src/languages/languages.ts
+++ src/languages/languages.ts
@@ -3,11 +3,16 @@ import { enUS } from "./enUS";
 import { esEs } from "./esEs";
 import { Language } from "./types/Language";
 
+import flagBR from "../assets/flag-br.svg";
+import flagUS from "../assets/flag-us.svg";
+import flagES from "../assets/flag-es.svg";
+
 type LanguageInfo = {
   [key: string]: {
     content: Language;
     name: string;
     flagAlt: string;
+    flagSvg: string;
     path: string;
   };
 };
@@ -17,18 +22,21 @@ export const languages: LanguageInfo = {
     content: ptBR,
     name: "Português (Brasil)",
     flagAlt: "Bandeira do Brasil",
+    flagSvg: flagBR,
     path: "?lang/pt-BR",
   },
   "en-US": {
     content: enUS,
     name: "English (United States)",
     flagAlt: "US Flag",
+    flagSvg: flagUS,
     path: "?lang/en-US",
   },
   "es-ES": {
     content: esEs,
     name: "Español (Espanha)",
     flagAlt: "Bandera española",
+    flagSvg: flagES,
     path: "?lang/es-ES",
   },
 };
